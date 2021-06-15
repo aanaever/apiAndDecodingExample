e6d3cab01b3979b5c448d70b35867135b1aaeddc
aanaever <nasty.ever18@gmail.com>
2021-06-15 16:03:19 +0300

    Initial Commit

---
+++ apiAndDecodingExample/apiAndDecodingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiAndDecodingExampleApp.swift
+//  apiAndDecodingExample
+//
+//  Created by Анастасия Эверстова on 15.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct apiAndDecodingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
